0264cc4e3b2380236aec416c12f2f0e9a3ad2868
duycop <duycop@gmail.com>
2024-01-08 22:53:20 +0700

    làm đẹp code

--- app/src/main/java/vn/edu/tnut/mysqlite/MainActivity.java
+++ app/src/main/java/vn/edu/tnut/mysqlite/MainActivity.java
@@ -10,6 +10,15 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     private DataDAO dataDAO;
+    final String hotens[] = {"Hùng", "Dũng", "Tuấn", "Tú"};
+
+    void init_db() {
+        //nếu lần đầu chạy app: thì bổ xung vài row mặc định
+        if (dataDAO.getDataCount() == 0) {
+            for (String hoten : hotens)
+                dataDAO.insertData(hoten);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,36 +28,39 @@ public class MainActivity extends AppCompatActivity {
         dataDAO = new DataDAO(this);
         dataDAO.open();
 
-        //nếu lần đầu chạy app: thì bổ xung vài row mặc định
-        if(dataDAO.getDataCount()==0){
-            final String hotens[]={"Hùng","Dũng","Tuấn","Tú"};
-            for (String hoten : hotens)
-                dataDAO.insertData(hoten);
-        }
+        //nếu lần đầu chạy app: thì bổ sung vài row mặc định
+        init_db();
+
         //list all ngay
         listAllrow(null);
     }
 
     @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        dataDAO.close();
+    protected void onDestroy() { //khi app bị die
+        super.onDestroy();       //gọi để huỷ lớp bố
+        dataDAO.close();         //giải phóng con trỏ ghi file sqlite
     }
 
     public void insert1row(View view) {
         //lấy họ tên nhập trên form
         EditText txtHoTen = findViewById(R.id.txtHoTen);
         String hoten = txtHoTen.getText().toString();
-        //gọi dataDAO để insert
 
+        //gọi dataDAO để insert
         dataDAO.insertData(hoten);
+
+        //view kq luôn
         listAllrow(view);
     }
 
     public void listAllrow(View view) {
         //gọi dataDAO để lấy mọi row -> string -> GUI
         String strAllRow = dataDAO.getAllDataAsString();
+
+        //lấy đối tượng trên giao diện chuẩn bị hiển thị
         TextView txtKQ = findViewById(R.id.txtKQ);
+
+        //gán text cho nó để hiển thị
         txtKQ.setText(strAllRow);
     }
 }
